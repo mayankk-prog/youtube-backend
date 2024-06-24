ad946c65feb6154e5487fd6c554e848a28ea80bd
mayankk-prog <bhmayank001@gmail.com>
2024-06-24 22:35:19 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import mongooose from "mongoose";
-import {DB_NAME} from "./contants";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    try {
+      app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is connected at ${process.env.PORT}`);
+      });
+    } catch (error) {
+      console.log("error in app.listen: ", error);
+      throw error;
+    }
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
+
+/*
+app.listen( process.env.PORT || 8000, () => {
+        console.log(` Server is connected at ${process.env.PORT}`);
+    });
 
 import express from "express";
 const   app= express()
@@ -22,3 +47,5 @@ const   app= express()
         throw err
     }
 })
+
+*/
