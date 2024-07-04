5a9c2594a08dbe55312af0c9d771f5bcc263a8ac
mayankk-prog <bhmayank001@gmail.com>
2024-07-04 22:36:47 +0530

    added refreshAccessToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -162,6 +162,50 @@ const logoutUser = asyncHandler(async(req, res) => {
   .json(new ApiResponse(200, {}, "user logged out"))
 })
 
+const refreshAccessToken= asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+  if(incomingRefreshToken){
+    throw new ApiError(401, "unauthorized request")
+  }
+
+  try {
+    const decodedToken= jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+  
+    const user= await User.findById(decodedToken?._id)
+    if (!user) {
+      throw new ApiError(401, "Invalid Refresh TOken");
+    }
+  
+    if (incomingRefreshToken !== user?.refreshToken ){
+      throw new ApiError(401, "refresh Token is expired or used")
+    }
+  
+  
+    const options= {
+      httpOnly: true,
+      secure: true
+    }
+  
+    const {accessToken, newRefreshToken}= await generateAccessAndRefreshTokens(user._id)
+  
+    return res
+    .status(200)
+    .cookie("accessToken", ac)
+    .cookie("refreshToken")
+    .json(
+      new ApiResponse(200, {accessToken, refreshToken: newRefreshToken}, "access token refreshed")
+    )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "invalid refresh Token")
+  }
+
+})
+
 export { registerUser,
 loginUser,
-logoutUser };
+logoutUser,
+refreshAccessToken };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,7 @@ import { upload } from "../middlewares/multer.middleware.js";
 import jwt from "jsonwebtoken";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 import { logoutUser } from "../controllers/user.controller.js";
-
+import { refreshAccessToken } from "../controllers/user.controller.js";
 const router = Router()
 
 router.route("/register").post(
@@ -17,5 +17,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT ,logoutUser)
+
+router.route("/refresh-Token").post(refreshAccessToken)
 
 export default router
