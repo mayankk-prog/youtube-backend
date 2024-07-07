2edaf7e4a68b6996377220c6c70a5f3e478d018b
mayankk-prog <bhmayank001@gmail.com>
2024-07-07 22:39:59 +0530

    adding new routes I am trying to add a new route to my application. I have a controller called "user"

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,52 @@
-import {Router} from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js"
+import { Router } from "express";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import jwt from "jsonwebtoken";
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import jwt, { verify } from "jsonwebtoken";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { logoutUser } from "../controllers/user.controller.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {name: "avatar", maxCount: 1},
-        {name: "coverImage", maxCount: 1}
-    ]), 
-    registerUser)
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
-router.route("/logout").post(verifyJWT ,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refresh-Token").post(refreshAccessToken)
+router.route("/refresh-Token").post(refreshAccessToken);
 
-export default router
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
+
+export default router;
